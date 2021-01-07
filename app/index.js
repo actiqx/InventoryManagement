36b6fd11d51c8e7e1f2cae9288968950b3e66117
priya4110 <priyatoshpatra@gmail.com>
2021-01-07 21:03:46 +0530

    cors

--- app/index.js
+++ app/index.js
@@ -3,10 +3,22 @@ const ErrorHandler = require("./middlewares/errorHandler");
 // import express from "express";
 require("dotenv").config();
 require("./db");
+const cors = require("cors");
 const productRoutes = require("./routes/product");
 const app = express();
 
 app.use(express.json());
+// For Reference
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS,GET,POST,PUT,PATCH,DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+//   next();
+// });
+app.use(cors());
 app.use(productRoutes);
 
 app.use(ErrorHandler);
